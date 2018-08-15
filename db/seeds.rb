b39c6ee7d94df8fbab967ac23d34be27e089a480
Maria SanchezBarrueta <mc.sanchez.4142@gmail.com>
2018-08-15 17:36:58 +0100

    all features except the shpw

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+puts 'Cleaning database...'
+Flat.destroy_all
+
+puts 'Creating flats...'
+flats_attributes = [
+  {
+    name:         'Charming loft',
+    address:      '7 Boundary St, London E2 7JE',
+    description:  'Buzzy destination for Indian street food in Bombay-style vintage decor.',
+    price_per_night:        50,
+    number_of_guests:       2
+  },
+  {
+    name:         'Awesome basement',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    description:  'Pizzeria with industrial looks, serving rustic pizza and antipasti.',
+    price_per_night:        60,
+    number_of_guests:       1
+  }
+]
+Flat.create!(flats_attributes)
+puts 'Finished!'
